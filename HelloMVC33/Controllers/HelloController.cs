c45d4fe924dc38f2720e023641e355898693b5c9
Scott Land <101.scott@gmail.com>
2017-03-04 13:03:19 -0600

    Save solution between videos.

---
+++ HelloMVC33/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC33.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("<h1>Hello World</h1>", "text/html");
+        }
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
