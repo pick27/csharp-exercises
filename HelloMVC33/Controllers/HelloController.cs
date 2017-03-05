84a514b09843c270dd36199345b89ec1f3896370
Scott Land <101.scott@gmail.com>
2017-03-05 15:02:23 -0600

    Saving after learning about Routes.

--- HelloMVC33/Controllers/HelloController.cs
+++ HelloMVC33/Controllers/HelloController.cs
@@ -7,10 +7,37 @@ namespace HelloMVC33.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            string html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+
+            //return Redirect("/Hello/Goodbye");
         }
+
+        //  /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // handle requests to /Hello/Chris  (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        //   /Hello/Goodbye
+        //  //   alter it to be /Hello/Aloha
+        //  [Route("/Hello/Aloha")]
         public IActionResult Goodbye()
         {
             return Content("Goodbye");
