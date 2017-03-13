73194b95acfe8ec0dedffe46864980874e538555
Scott Land <101.scott@gmail.com>
2017-03-12 21:14:46 -0500

    Added Student project as requested.

--- HelloMVC33/Controllers/HelloController.cs
+++ HelloMVC33/Controllers/HelloController.cs
@@ -12,6 +12,13 @@ namespace HelloMVC33.Controllers
         {
             string html = "<form method='post'>" +
                 "<input type='text' name='name' />" +
+                "<select name='language'>" +
+                "<option>English</option>" +            // hello
+                "<option>French</option>" +             // bonjour
+                "<option>Spanish</option>" +            // hola
+                "<option>German</option>" +             // Hallo
+                "<option>Latin</option>" +              // Salve 
+                "</select>" +
                 "<input type='submit' value='Greet me!' />" +
                 "</form>";
             return Content(html, "text/html");
@@ -19,12 +26,62 @@ namespace HelloMVC33.Controllers
             //return Redirect("/Hello/Goodbye");
         }
 
+        public static string CreateMessage(string name, string language)
+        {
+            string message = "";
+            
+            if (language == "English")
+            {
+                message = "Hello";
+            }
+            else if (language == "French")
+            {
+                message = "Bonjour";
+            }
+            else if (language == "Spanish")
+            {
+                message = "Hola";
+            }
+            else if (language == "German")
+            {
+                message = "Hallo";
+            }
+            else if (language == "Latin")
+            {
+                message = "Salve";
+            }
+
+            message = message + " " + name;
+
+            return (message);
+        }
+
         //  /Hello
         [Route("/Hello")]
         [HttpPost]
-        public IActionResult Display(string name = "World")
+        public IActionResult Display(string name = "World", string language = "English")
         {
-            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+            int visits = 0;
+            string temp = "";
+            bool success;
+            success = Request.Cookies.TryGetValue("bonjour", out temp);
+            if (temp == null || temp.Length == 0)
+            {
+                temp = "0";
+            }
+            visits = int.Parse(temp) + 1;
+            string whatToSay = "";
+            if (name == "")
+                name = "World";
+            whatToSay = CreateMessage(name, language);
+            whatToSay = "<h1>We hope this is what you asked for</h1>" +
+                "<h2><b>" +
+                whatToSay +
+                "</b></h2>" +
+                "<b>How many visits: " + visits + "</b>";
+
+            Response.Cookies.Append("bonjour", visits.ToString());
+            return Content(whatToSay, "text/html");
         }
 
         // handle requests to /Hello/Chris  (URL segment)
---
+++ School/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Student
+    {
+        string name;
+        int studentId;
+        int numberOfCredits;
+        double gpa;
+
+        public string Name { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int NumberOfCredits
+        {
+            get { return numberOfCredits; }
+            internal set { numberOfCredits = value; }
+        }
+
+        public double Gpa
+        {
+            get { return gpa; }
+            internal set { gpa = value; }
+        }
+    }
+
+
+    public class Course
+    {
+        string name;
+        int courseId;
+        int numberOfCredits;
+        string instructor;
+        Student[] students;
+
+        public string Name { get; set; }
+
+        public int CourseId { get; set; }
+
+        public int NumberOfCredits
+        {
+            get { return numberOfCredits; }
+            internal set { numberOfCredits = value; }
+        }
+
+        public string Instructor { get; set; }
+
+        public Student[] Students
+        {
+            get { return students; }
+            internal set { students = value; }
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("just checking to see if it compiles.");
+            Console.WriteLine("but that doesn't mean much.");
+
+            Console.ReadLine();
+        }
+    }
+}
