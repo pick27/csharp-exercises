b12cb7d13994082499227583f926ac5e410cd7ea
Scott Land <101.scott@gmail.com>
2017-03-20 16:49:56 -0500

    It has been a while.

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -8,15 +8,31 @@ namespace Restaurant
 {
     public class Menu
     {
-        private static DateTime latestUpdate;
-        private static List<MenuItem> itemsOnMenu;
+        protected static DateTime latestUpdate;
+        protected static List<MenuItem> itemsOnMenu;
+        protected static List<MenuItem> allItems;
 
         public DateTime LatestUpdate { get; set; }
         public List<MenuItem> ItemsOnMenu { get; set; }
+        public List<MenuItem> AllItems { get; set; }
 
+        public Menu()
+        {
+            latestUpdate = DateTime.Now;
+            itemsOnMenu = new List<MenuItem>();
+            allItems = new List<MenuItem>();
+        }
         public void displayMenu()
         {
             StringBuilder wholeMenu = new StringBuilder();
+            // last updated
+
+            // appetizers
+
+            // main courses
+
+            // desserts
+
             Console.WriteLine(wholeMenu);
         }
 
@@ -36,21 +52,39 @@ namespace Restaurant
 
     public class MenuItem
     {
+        protected static int nextUniqueId = 1;
+
         private string name;
-        private double price;
-        private string category;
+        private decimal price;
+        private string category; // appetizer, main course, dessert
         private bool newItem;
         private bool onMenu;
+        private int uniqueId;
         private DateTime lastUpdate;
 
         public string Name { get; set; }
         public double Price { get; set; }
-        public string Category { get; set;  }
+        public string Category { get; set; }
         public bool NewItem { get; set; }
         public bool OnMenu { get; set; }
         public DateTime LastUpdate { get; set; }
 
-        public void editMenuItem()
+        public MenuItem()
+        {
+            category = "main course";
+            onMenu = false;
+            newItem = false;
+        }
+
+        public void addMenuItem()
+        {
+            MenuItem menuItem = new MenuItem();
+            uniqueId = nextUniqueId;
+            nextUniqueId++;
+            editMenuItem(menuItem.uniqueId);
+        }
+
+        public void editMenuItem(int uniqId)
         {
 
         }
@@ -60,7 +94,13 @@ namespace Restaurant
 
         }
 
-        public void deleteMenuItem()
+        public void displayMenuItem()
+        {
+            Console.WriteLine("name" + item.name);
+            Console.WriteLine("------------------------\n");
+        }
+
+        public void deleteMenuItem(MenuItem item)
         {
             // only let them remove items that are not on Menu
         }
@@ -71,6 +111,71 @@ namespace Restaurant
     {
         public static void Main(string[] args)
         {
+            string thingsToDo = "1) display menu \n2) display all menu items \n3) display a menu item \n4) add a menu item \n5) edit a menu item \n6) add item to menu \n7) remove item from menu \n8) remove item from list of menu items \n0) quit/exit the program \n";
+            string done = "99";
+            Console.WriteLine("About to try to create Menu object.");
+            Menu menu = new Menu();
+            Console.WriteLine("Hey it was created.");
+
+            while (done != "0")
+            {
+                Console.WriteLine(thingsToDo);
+                done = Console.ReadLine();
+                if (done == "1")
+                {
+                    menu.displayMenu();
+                }
+                else if (done == "2")
+                {
+                    foreach(MenuItem item in menu.AllItems)
+                    {
+                        item.displayMenuItem();
+                    }
+                }
+                else if (done == "3")
+                {
+                    // display item name/number to choose from
+                    MenuItem item = new MenuItem();
+                    item.displayMenuItem();
+                }
+                else if (done == "4")
+                {
+                    MenuItem item = new MenuItem();
+                    item.addMenuItem();
+                    menu.AllItems.Add(item);
+                }
+                else if (done == "5")
+                {
+                    // display item name/number to choose from
+                    MenuItem item = new MenuItem();
+                }
+                else if (done == "6")
+                {
+                    // display item name/number to choose from
+                    MenuItem item = new MenuItem();
+                    menu.addItemToMenu(item);
+                }
+                else if (done == "7")
+                {
+                    // display item name/number to choose from
+                    MenuItem item = new MenuItem();
+                    menu.ItemsOnMenu.Remove(item);
+                }
+                else if (done == "8")  // remove item from list of menu items
+                {
+                    // display item name/number to choose from
+                    MenuItem item = new MenuItem();
+                    menu.AllItems.Remove(item);
+                }
+                else if (done == "0")
+                {
+                    Console.WriteLine("Thanks for testing the program");
+                }
+                else
+                {
+                    Console.WriteLine("what you talking about Willis.");
+                }
+            }
         }
     }
 }
---
+++ Shape37/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shapes37
+{
+    public class AbstractEntity
+    {
+        private int id;
+        private static int nextUniqueId = 1;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public AbstractEntity()
+        {
+            id = nextUniqueId;
+            nextUniqueId++;
+        }
+    }
+
+    public class Shape:AbstractEntity
+    {
+        // didn't want to make a point type too
+        private string location;
+
+        public string Location
+        {
+            get { return this.location; }
+            set { this.location = value; }
+        }
+
+        public Shape(string location)
+        {
+            Location = location;
+        }
+
+        //  public virtual double area() { }
+        //  public virtual double perimeter() { }
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get { return this.radius; }
+            set { this.radius = value; }
+        }
+
+        public Circle(string location, double radius) : base(location)
+        {
+            Radius = radius;
+        }
+
+        public double area()
+        {
+            return (Math.PI * Radius * Radius);
+        }
+
+        //  public double perimeter()
+        //  {
+        //      return (2 * Math.PI * Radius);
+        //  }
+
+        public override string ToString()
+        {
+            return "Type: Circle; Radius: " + Radius;
+        }
+    }
+
+
+
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public double Length
+        {
+            get { return this.length; }
+            set { this.length = value; }
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        public Rectangle(string location, double length, double width) : base(location)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public virtual double area()
+        {
+            return (Length * Width);
+        }
+
+        //  public double perimeter()
+        //  {
+        //      return (2 * (Length + Width));
+        //  }
+
+        public override string ToString()
+        {
+            return "Type: Rectangle; Length: " + Length + "; Width: " + Width;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(string location, double side) : base(location, side, side)
+        {
+            // base class will enter values
+        }
+
+        public override double area()
+        {
+            return (Length * Width);
+        }
+
+        //  public double perimeter()
+        //  {
+        //      return (4 * Length);
+        //  }
+
+        public override string ToString()
+        {
+            return "Type: Square; Side: " + Length;
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle a = new Circle("0,0", 3.14);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine("My unique Id is: " + a.Id + ";  My type is:" + a.GetType());
+            Console.WriteLine("area of circle: " + a.area());
+
+            Rectangle b = new Rectangle("1,1", 3.14, 4.13);
+            Console.WriteLine(b.ToString());
+            Console.WriteLine("My unique Id is: " + b.Id + ";  My type is:" + b.GetType());
+            Console.WriteLine("area of rectangle: " + b.area());
+
+            Square c = new Square("2,2", 4.13);
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("My unique Id is: " + c.Id + ";  My type is:" + c.GetType());
+            Console.WriteLine("area of square: " + c.area());
+
+            Console.ReadLine();
+        }
+    }
+}
